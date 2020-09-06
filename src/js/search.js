193c8a109384a0abb20ed05e16d994a1b5c8057e
Александра <kruglyakova.sasha@gmail.com>
2020-09-06 23:59:27 +0300

    Add search functionality

--- src/js/search.js
+++ src/js/search.js
@@ -1,5 +1,10 @@
-import startApp from './script';
 import getWeather from './weather';
+import getUserLocation from './location';
+
+export default function initSearchListeners() {
+  initInputSearch();
+  initLocationSearch();
+}
 
 function initInputSearch() {
   const searchInput = document.querySelector('[data-search-input]');
@@ -8,19 +13,34 @@ function initInputSearch() {
   ymaps.ready(init);
 
   function init() {
-      var suggestView1 = new ymaps.SuggestView(searchInput);
+      let suggestView1 = new ymaps.SuggestView(searchInput);
   }
+
+  searchForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const searchValue = searchInput.value;
+
+    if (!searchValue) return;
+
+    const response = await fetch(`https://geocode-maps.yandex.ru/1.x?geocode=${searchValue}&apikey=cd0bd47c-5f52-4228-b580-3cde6b7d8c6b&format=json&results=1`);
+    const result = await response.json();
+
+    const coords = result.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos;
+
+    const weatherResults = await getWeather(...coords.split(' ').reverse().map((i) => +i));
+
+    console.log(weatherResults);
+  });
 }
 
 function initLocationSearch() {
   const locationButton = document.querySelector('[data-location-button]');
 
-  locationButton.addEventListener('click', () => {
-    startApp();
-  });
-}
+  locationButton.addEventListener('click', async () => {
+    const location = await getUserLocation();
+    const weather = await getWeather(location.latitude, location.longitude);
 
-export default function initSearchListeners() {
-  initInputSearch();
-  initLocationSearch();
+    console.log(weather);
+  });
 }
